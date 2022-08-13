62c02f90109b8b18a9cbb7c8a227528f519acaef
b21geoyo <b21geoyo@student.his.se>
2022-08-13 16:31:14 +0200

    Added code to read data from SharedPreferences when exiting SecondActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        String string = preferences.getString("text", "Empty");
+
+        text = findViewById(R.id.textview);
+        text.setText(string);
     }
 }
